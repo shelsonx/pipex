e3b8326a4a3e5b4fa2311bb111869d540448736c
sjhony-x <sjhony-x@student.42sp.org.br>
2022-08-15 22:48:49 +0200

    execv

---
+++ learning/ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("PID of ex1.c= %d\n", getpid());
+	char	*args[] = {"Hello", "42", "SP", NULL};
+	execv("./ex03", args);
+	printf("Back to ex01.c");
+	return (0);
+}
