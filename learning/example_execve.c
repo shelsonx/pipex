347a40760dfc638bf124cdb685164b705690b36d
sjhony-x <sjhony-x@student.42sp.org.br>
2022-08-15 19:30:25 +0200

    example execve

---
+++ learning/example_execve.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	char	*argv[] = {"/bin/ls", "-l", NULL};
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	if (pid == 0)
+	{
+		int ret = execve(argv[0], argv, NULL);
+		if (ret == -1)
+			perror("ERROR");
+	}
+	else
+	{
+		wait(NULL);
+		printf("Done with execve\n");
+	}
+}
