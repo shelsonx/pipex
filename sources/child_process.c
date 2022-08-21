ca35929e5e698409b5daedcf4c14d0f7f1c0821a
sjhony-x <sjhony-x@student.42sp.org.br>
2022-08-21 07:33:58 +0200

    create child_process.c

---
+++ sources/child_process.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   child_process.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjhony-x <sjhony-x@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/21 05:41:02 by sjhony-x          #+#    #+#             */
+/*   Updated: 2022/08/21 07:31:09 by sjhony-x         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+pid_t	create_child_process(void (*func)(t_data), t_data data)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("pipex");
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		func(data);
+	}
+	return (pid);
+}
