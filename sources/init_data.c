a3e5fa71f38deaf6720d8987b6caec99e51f4964
shelson <shelsonx@gmail.com>
2022-08-22 17:30:51 -0300

    personalized message file error

--- sources/init_data.c
+++ sources/init_data.c
@@ -15,11 +15,14 @@
 t_data	get_data_first_cmd(char **argv, char **envp, int *fd)
 {
 	t_data	data;
+	char	*msg;
 
 	data.file_fd = open(argv[1], O_RDONLY);
 	if (data.file_fd < 0)
 	{
-		perror("pipex");
+		msg = ft_strjoin("pipex: ", argv[1]);
+		perror(msg);
+		free(msg);
 		exit(0);
 	}
 	data.first_redirect_pipe = fd[0];
@@ -34,11 +37,14 @@ t_data	get_data_first_cmd(char **argv, char **envp, int *fd)
 t_data	get_data_last_cmd(int argc, char **argv, char **envp, int *fd)
 {
 	t_data	data;
+	char	*msg;
 
 	data.file_fd = open(argv[argc -1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
 	if (data.file_fd < 0)
 	{
-		perror("pipex");
+		msg = ft_strjoin("pipex: ", argv[argc -1]);
+		perror(msg);
+		free(msg);
 		exit(0);
 	}
 	data.first_redirect_pipe = fd[1];
